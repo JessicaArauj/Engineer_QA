ae5764cfa6c10ef9ae9306a97d84aafeaba2adf1
Jessica Teixeira Araujo <jessicateixeiraaraujo@gmail.com>
2023-01-22 19:39:19 -0300

    criando enderecoManual.cy.js e endereco.manual.js

--- Teste_UI_LOJA_EBAC/cypress/e2e/endereco.cy.js
+++ Teste_UI_LOJA_EBAC/cypress/e2e/endereco.cy.js
@@ -2,8 +2,6 @@
 
 import enderecoPage from '../support/page-objects/endereco.page'
 
-const dadosEndereco = require('../fixtures/endereco.json')
-
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     before(() => {
         cy.visit('minha-conta')
@@ -15,24 +13,6 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     it('Deve fazer cadastro de faturamento com sucesso', () => {
         enderecoPage.editarEnderecoFaturamento()
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
-    });
-
-    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
-        enderecoPage.editarEnderecoFaturamento(
-            dadosEndereco[1].nome,
-            dadosEndereco[1].sobrenome,
-            dadosEndereco[1].empresa,
-            dadosEndereco[1].pais,
-            dadosEndereco[1].endereco,
-            dadosEndereco[1].numero,
-            dadosEndereco[1].cidade,
-            dadosEndereco[1].estado,
-            dadosEndereco[1].cep,
-            dadosEndereco[1].telefone,
-            dadosEndereco[1].email
-            )
-        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso')
-
     });
 
 });
